93639c5c32c3f3ae73378bb2617c5a89ab996fda
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-12-06 11:12:32 +0530

    just done with js movable cusror code but also going with GSAP

---
+++ script.js
@@ -0,0 +1,7 @@
+let page1Content = document.querySelector(".page1_content")
+let cursor = document.querySelector("#cursor")
+
+page1Content.addEventListener("mousemove" , function(dets){
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
+})
