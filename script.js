73c7addc66a17259e43102ee217a5d473a0048a4
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-12-06 11:21:10 +0530

    used the GSAP for movaable cursor

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let page1Content = document.querySelector(".page1_content")
 let cursor = document.querySelector("#cursor")
 
 page1Content.addEventListener("mousemove" , function(dets){
-    cursor.style.left = dets.x + "px";
-    cursor.style.top = dets.y + "px";
+    // cursor.style.left = dets.x + "px";
+    // cursor.style.top = dets.y + "px";
+   gsap.to(cursor , {
+    x: dets.x,
+    y: dets.y
+   })
 })
