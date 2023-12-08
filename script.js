33b8c27aecf0daa2944c1f754eeb81f5570268af
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-12-08 12:51:56 +0530

    added autoplay scripts

--- script.js
+++ script.js
@@ -115,12 +115,9 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:true,
+    
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+  });
